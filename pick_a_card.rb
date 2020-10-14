704961a95722f091700020646f1f2634fd1a9653
quietpochatok <bosroin@gmail.com>
2020-10-14 15:31:17 +0300

    refactor: change card suit

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -4,7 +4,7 @@ values = %w[2 3 4 5 6 7 8 9 10 J Q K A]
 
 # Создадим массив с набором мастей.
 # Буби, черви, крести, пики.
-suits = %w[Diamonds Hearts Clubs Spades]
+suits = %w[♦ ♥ ♣ ♠]
 
 # Выведем произвольную карты, выбрав по одному элементу из массивов
 puts "#{values.sample} of #{suits.sample}"
